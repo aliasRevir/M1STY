09f461fc885326ae4fb30e2368dc37ec80fdf9e2
Xi'En <79648987+XiEn1847@users.noreply.github.com>
2021-10-12 08:32:54 +0800

    0.9

--- M1STY.cpp
+++ M1STY.cpp
@@ -1,170 +1,133 @@
 /*
-M1STY ver 0.8
+M1STY ver 0.9
 
-README
-
-按 Alt + 0 ~ 9 对当前窗口进行不同程度的透明化
-按 Alt + '+' 或 '-' 微调
-按 Alt + '~' 禁止调整某个窗口，并把它变回不透明
-
-单击这个窗口之后，按 Alt + '\' 将其隐藏。
-** 注意！！！只能隐藏，不能显示，而且不保证对其他窗口无效！！！
-
-按 Alt + 退格(backspace)结束程序
-** 这在任何情况下有效。
-
+By XiEn1847
+Finished at [2021/10/11 22:01]
 */
-
 
 #include <stdio.h>
 #include <conio.h>
-
-// #define _WIN32_WINNT 0x0601
-// Windows 7
 #include <windows.h>
+
 
 using namespace std;
 
 
-int main() {
-    RECT rect;
-    HWND hwnd=NULL,oldhwnd=NULL;
-    int a=0,b=0,proc=0,hide=0;
-    int spamA=0,spamB=0;
+int main(){
+	RECT rect;
+	HWND hwnd=NULL,oldhwnd=NULL;
 
-    int num[10]= {0};
-    int numAlpha[10]= {1+254*0/9,1+254*1/9,1+254*2/9,1+254*3/9,1+254*4/9,1+254*5/9,1+254*6/9,1+254*7/9,1+254*8/9,1+254*9/9};
-    int Alpha=255;
-
-    puts("Press Alt + {Num 0~9} to Set Opacity.\n");
-    puts("Press Alt + {+} / {-} to Increase / Decrease Opacity.\n");
-    puts("Press Alt + {~} to Protect a Window From Control.");
-    puts(" >> If You Want to Control It Again, Press Alt and Switch Back From Another Window.\n");
-
-    // finish this ...
-    // finished [08/05 20:34]
-    puts("Press Alt + {\\} to Hide this Window.");
-    puts(" >> Once Hidden, It Can't Shown Again.\n");
-    puts("Press Alt + Backspace to Exit.");
-    //
-
-    while(1) {
-        /*
-        hwnd=GetForegroundWindow();
-        if(hwnd!=oldhwnd) {
-            DWORD dwExStyle=GetWindowLong(hwnd,GWL_EXSTYLE);
-            if(!(dwExStyle&0x80000))
-                SetWindowLong(hwnd,GWL_EXSTYLE,dwExStyle^0x80000);
-            GetWindowRect(hwnd, &rect);
-
-            x=rect.left;
-            y=rect.top;
-            width=rect.right-x;
-            height=rect.bottom-y;
-            oldhwnd=hwnd;
-        }
-        */
-        Sleep(1);
-
-        if(GetKeyState(18)<0) { // Alt
-            hwnd=GetForegroundWindow();
-            if(hwnd!=oldhwnd) {
-                DWORD dwExStyle=GetWindowLong(hwnd,GWL_EXSTYLE);
-                if(!(dwExStyle&0x80000))
-                    SetWindowLong(hwnd,GWL_EXSTYLE,dwExStyle^0x80000);
-                GetWindowRect(hwnd, &rect);
-                oldhwnd=hwnd;
-            }
-
-            a=GetKeyState(187);
-            if(a<0) {
-                if(spamA==0) {
-                    if(Alpha<=239)Alpha+=16;
-                    else Alpha=255;
-                    SetLayeredWindowAttributes(hwnd, 0, Alpha, LWA_ALPHA);
-
-                    spamA=1;
-                    do {
-                        a=GetKeyState(187);
-                        spamA++;
-                        if(spamA>=500000) {
-                            spamA-=10000;
-                            if(Alpha<=254)Alpha+=1;
-                            else Alpha=255;
-                            SetLayeredWindowAttributes(hwnd, 0, Alpha, LWA_ALPHA);
-                        }
-                    } while(a<0);
-                }
-            } else {
-                spamA=0;
-            }
-            b=GetKeyState(189);
-            if(b<0) {
-                if(spamB==0) {
-                    if(Alpha>=17)Alpha-=16;
-                    else Alpha=1;
-                    SetLayeredWindowAttributes(hwnd, 0, Alpha, LWA_ALPHA);
-
-                    spamB=1;
-                    do {
-                        b=GetKeyState(189);
-                        spamB++;
-                        if(spamB>=500000) {
-                            spamB-=10000;
-                            if(Alpha>=2)Alpha-=1;
-                            else Alpha=1;
-                            SetLayeredWindowAttributes(hwnd, 0, Alpha, LWA_ALPHA);
-                        }
-                    } while(b<0);
-                }
-            } else {
-                spamB=0;
-            }
-
-            for(int i=0; i<=9; i++) {
-                num[i]=GetKeyState(48+i);
-                if(num[i]<0) {
-                    Alpha=numAlpha[i];
-                    SetLayeredWindowAttributes(hwnd, 0, Alpha, LWA_ALPHA);
-                    do {
-                        num[i]=GetKeyState(48+i);
-                    } while(num[i]<0);
-
-                }
-            }
-
-            proc=GetKeyState(192);
-            if(proc<0) {
-
-                DWORD dwExStyle=GetWindowLong(hwnd,GWL_EXSTYLE);
-                if((dwExStyle&0x80000))
-                    SetWindowLong(hwnd,GWL_EXSTYLE,dwExStyle^0x80000);
-                do {
-                    proc=GetKeyState(192);
-
-                } while(proc<0);
-            }
-
-            hide=GetKeyState(220);
-            if(hide<0) {
-
-                ShowWindow(hwnd,false);
-                DWORD dwExStyle=GetWindowLong(hwnd,GWL_EXSTYLE);
-
-                SetWindowLong(hwnd, GWL_EXSTYLE, ( dwExStyle | WS_EX_TOOLWINDOW ) & WS_EX_APPWINDOW);
-
-                do {
-                    hide=GetKeyState(220);
-
-                } while(hide<0);
-            }
-
-            if(GetKeyState(8)<0) { // Backspace
-                goto EXIT;
-            }
-        }
-    }
-    EXIT:;
-    puts("Process Exited.\n");
-    return 0;
+	int numkey[4]={48,49,56,57}; // '0','1','8','9'
+	int numAlpha[10]={1+254*0/9,1+254*1/9,1+254*8/9,1+254*9/9};
+	int Alpha=255;
+	puts("Press Alt + Num { 0,1,8,9 } to Set Opacity.\n");
+	puts("Press Alt + { + } / { - } to Increase / Decrease Opacity.\n");
+	puts("Press Alt + { ~ } to Protect a Window From Control.");
+	puts(" >> If You Want to Control It Again, Press Alt and Switch Back From Another Window.\n");
+	puts("Press Alt + { [ } to Enable/Disable Clicking Through the Window.");
+	puts("Press Alt + { ] } to Enable/Disable Always-on-Top.");
+	// finish this ...
+	// finished [08/05 20:34]
+	// added '[' and ']' at [10/11 22:01]
+	puts("Press Alt + { \\ } to Hide this Window.");
+	puts(" >> Once Hidden, It Can't be Shown Again.\n");
+	puts("Press Alt + Backspace to Exit.");
+	while(1){
+		Sleep(1);
+		// Key 'Alt'
+		if(GetKeyState(18)<0){ 
+			hwnd=GetForegroundWindow();
+			if(hwnd!=oldhwnd){
+				Alpha=255;
+				DWORD dwExStyle=GetWindowLong(hwnd,GWL_EXSTYLE);
+				if(!(dwExStyle&0x80000))
+					SetWindowLong(hwnd,GWL_EXSTYLE,dwExStyle|0x80000);
+				GetWindowRect(hwnd,&rect);
+				oldhwnd=hwnd;
+			}
+			// Key '+' Increases Alpha
+			if(GetKeyState(187)<0){
+				int spamA=0;
+				if(Alpha<=252)Alpha+=3;
+				else Alpha=255;
+				SetLayeredWindowAttributes(hwnd,0,Alpha,LWA_ALPHA);
+				do{
+					spamA++;
+					if(spamA>=500000){
+						spamA-=17000;
+						if(Alpha<=254)Alpha+=1;
+						else Alpha=255;
+						SetLayeredWindowAttributes(hwnd,0,Alpha,LWA_ALPHA);
+					}
+				}while(GetKeyState(187)<0);
+			}
+			//
+			// Key '-' Decreases Alpha
+			if(GetKeyState(189)<0){
+				int spamB=0;
+				if(Alpha>=4)Alpha-=3;
+				else Alpha=1;
+				SetLayeredWindowAttributes(hwnd,0,Alpha,LWA_ALPHA);
+				do{
+					spamB++;
+					if(spamB>=500000){
+						spamB-=17000;
+						if(Alpha>=2)Alpha-=1;
+						else Alpha=1;
+						SetLayeredWindowAttributes(hwnd,0,Alpha,LWA_ALPHA);
+					}
+				}while(GetKeyState(189)<0);
+			}
+			//
+			// Key '0','1','8','9' Sets Alpha
+			for(int i=0;i<=3;i++) {
+				if(GetKeyState(numkey[i])<0){
+					Alpha=numAlpha[i];
+					SetLayeredWindowAttributes(hwnd,0,Alpha,LWA_ALPHA);
+					do{}while(GetKeyState(numkey[i])<0);
+				}
+			}
+			//
+			// Key '~' Disables Changing
+			if(GetKeyState(192)<0){
+				DWORD dwExStyle=GetWindowLong(hwnd,GWL_EXSTYLE);
+				if(dwExStyle&0x80000)
+					SetWindowLong(hwnd,GWL_EXSTYLE,dwExStyle^0x80000);
+				do{}while(GetKeyState(192)<0);
+			}
+			//
+			// Key '\' Hides The Window
+			if(GetKeyState(220)<0){
+				ShowWindow(hwnd,false);
+				DWORD dwExStyle=GetWindowLong(hwnd,GWL_EXSTYLE);
+				SetWindowLong(hwnd,GWL_EXSTYLE,(dwExStyle|WS_EX_TOOLWINDOW)&WS_EX_APPWINDOW);
+				do{}while(GetKeyState(220)<0);
+			}
+			//
+			// Key '[' Enables/Disables Clicking Through the Window
+			if(GetKeyState(219)<0){
+				DWORD dwExStyle=GetWindowLong(hwnd,GWL_EXSTYLE);
+				SetWindowLong(hwnd,GWL_EXSTYLE,dwExStyle^0x20L);
+				do{}while(GetKeyState(219)<0);
+			}
+			//
+			// Key ']' Enables/Disables Always-on-Top
+			if(GetKeyState(221)<0){
+				if(GetWindowLong(hwnd,GWL_EXSTYLE)&WS_EX_TOPMOST){
+					// The window is topmost.
+					SetWindowPos(hwnd,HWND_NOTOPMOST,0,0,0,0,SWP_NOMOVE|SWP_NOSIZE);
+				}else{
+					// The window is not topmost.
+					SetWindowPos(hwnd,HWND_TOPMOST,0,0,0,0,SWP_NOMOVE|SWP_NOSIZE);
+				}
+				do{}while(GetKeyState(221)<0);
+			}
+			//
+			// Key 'Backspace' Exits The Program
+			if(GetKeyState(8)<0){goto EXIT;}
+		}
+	}
+	EXIT:;
+	puts("Process Exited.\n");
+	return 0;
 }
--- README.md
+++ README.md
@@ -6,11 +6,15 @@ May not Work for Specific Window.
 
 ## Hotkeys
 
-+ `Alt`+`0`~`9`: Set Current Window's Opacity ( 0 < 1 < ... < 8 < 9 )
++ `Alt`+`0`/`1`/`8`/`9`: Set Current Window's Opacity ( 0 < 1 < 8 < 9 )
 
 + `Alt`+`+`/`-`: Increase / Decrease Current Window's Opacity
 
 + `Alt`+`~`: Forbid Changing of this Window
+
++ `Alt`+`]`: Enable / Disable Always-on-Top
+
++ `Alt`+`[`: Enable / Disable Clicking Through Current Window
 
 + `Alt`+`\`: Hide Current Window ( Can't be Undone!! You have to Use `taskmgr`/`taskkill` to Close a Hidden Window )
 
@@ -28,17 +32,26 @@ May not Work for Specific Window.
 
 ## Hotkeys
 
-+ `Alt`+`0`~`9`: 设置当前窗口的不透明度 ( 0 < 1 < ... < 8 < 9 )
++ `Alt`+`0`/`1`/`8`/`9`: 设置当前窗口的不透明度 ( 0 < 1 < 8 < 9 )
 
 + `Alt`+`+`/`-`: 微调当前窗口的不透明度
 
 + `Alt`+`~`: 对当前窗口禁用透明度调整
 
-+ `Alt`+`\`: 隐藏当前窗口 ( 无法恢复！ 你得用 `taskmgr` / `taskkill` 来关掉一个被隐藏的窗口 )
++ `Alt`+`]`: 开启 / 关闭置顶
+
++ `Alt`+`[`: 开启 / 关闭允许鼠标穿透 ( 点击会被发送到下面的窗口 )
+
++ `Alt`+`\`: 隐藏当前窗口 ( 无法恢复！ 你得用 `taskmgr`/`taskkill` 来关掉一个被隐藏的窗口 )
 
 + `Alt`+`Backspace`: 退出程序 ( 即使它被隐藏了也有效 )
 
 
 ---
 
-~~挺好用的~~
+应灭火器的要求加上了 置顶 和 鼠标穿透 功能。
+
+删掉了 `Alt`+`2`~`7` 的热键，因为比较常用。
+
+~~为啥有人会用这个（~~
+
